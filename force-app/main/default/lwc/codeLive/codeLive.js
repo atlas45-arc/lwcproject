3f40ec52ac15a54ce5a4f4a1b753e740ca030b5f
atlas45-arc <arturwieczorek45@gmail.com>
2023-03-02 12:28:19 +0100

    init

---
+++ force-app/main/default/lwc/codeLive/codeLive.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class CodeLive extends LightningElement {
+    greeting='Hello World';
+    title='Code live propety';
+    childTitle='Code Live Child property';
+}
