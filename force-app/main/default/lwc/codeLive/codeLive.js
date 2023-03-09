4f36f5d05b84e29fed7854e131daf14da4ba2376
atlas45-arc <arturwieczorek45@gmail.com>
2023-03-09 05:39:20 +0100

    com1

--- force-app/main/default/lwc/codeLive/codeLive.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CodeLive extends LightningElement {
-    greeting='Hello World';
-    title='Code live propety';
-    childTitle='Code Live Child property';
-}
--- force-app/main/default/lwc/codeLive2/codeLive2.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class CodeLive2 extends LightningElement {
-    @api childTitleName;
-    // @api = the abiliity to set a this componets property from
-    // the html that includes this component
-    
-
-}
